12d415a06cb3a9ab6eea796032e8c1e71cc05fb6
AlexBC <alex.bo.1969@gmail.com>
2022-04-24 15:41:31 +0200

    for loop snippets

---
+++ forloop.ps1
@@ -0,0 +1,5 @@
+for ($counter = 1; $counter -le 100; $counter++ )
+{
+		# ADD YOUR CODE HERE
+    Write-Progress -Activity "Update Progress" -Status "$counter% Complete:" -PercentComplete $counter;
+}
